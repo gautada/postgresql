e3d1d195a6b2588d1352af81fd01bdf33491a16c
Adam T. Gautier <adam@gautier.org>
2022-06-17 20:13:52 -0400

    Healthcheck update

--- hc-pgweb.sh
+++ hc-pgweb.sh
@@ -5,7 +5,7 @@ HEALTH=0
 
 # Check #1 - A psql client can connect
 # TEST="$(/usr/bin/curl http://postgres.data.svc.cluster.local:8080)"
-TEST="$(/usr/bin/curl http://127.0.0.1:8080)"
+TEST="$(/usr/bin/curl --output /dev/null --silent http://127.0.0.1:8080)"
 if [ ! $? -eq 0 ] ; then
  HEALTH=1
 fi
--- hc-postgres.sh
+++ hc-postgres.sh
@@ -26,7 +26,7 @@ fi
 # fi
 
 # Check #3 - A psql client can connect
-TEST=$(/usr/local/pgsql/bin/psql -c "SELECT datname FROM pg_database;")
+TEST=$(/usr/bin/psql -c "SELECT datname FROM pg_database;")
 if [ $? -eq 1 ] ; then
  HEALTH=1
 fi
